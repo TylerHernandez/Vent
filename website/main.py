5c189873389601af9ac8367dfb88578f7d644b3e
Charles E Baiden <baidench@shu.edu>
2019-09-28 13:23:51 -0400

    init files

---
+++ website/main.py
@@ -0,0 +1,33 @@
+import webapp2
+import jinja2
+import os
+
+
+jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+class IntroPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        template = jinja_env.get_template("/templates/index.html")
+
+        self.response.write(template.render(template_vars))
+
+    def post(self):
+        template = jinja_env.get_template('/templates/index.html')
+
+
+
+
+
+
+
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', IntroPage)
+], debug=True)
